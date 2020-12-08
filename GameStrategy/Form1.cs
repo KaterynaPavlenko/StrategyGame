4ed44822fd24f6c849d5f3512f093c172492dea9
kateryna <pavlenkokateryna2000@gmail.com>
2020-12-08 14:11:00 +0200

    first commit

---
+++ GameStrategy/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GameStrategy.Controller;
+
+namespace GameStrategy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        public void Init()
+        {
+        }
+    }
+}
